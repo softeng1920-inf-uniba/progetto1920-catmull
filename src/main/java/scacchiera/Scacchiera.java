aa7a12f7e3cae16de064793b1ad6d0893ae16694
marck9918 <marcofusillo99@outlook.com>
2020-04-10 17:13:33 +0200

    Aggiunta classe Main e eliminazione package con file di esempio

--- src/main/java/scacchiera/Scacchiera.java
+++ src/main/java/scacchiera/Scacchiera.java
@@ -8,6 +8,7 @@ public class Scacchiera {
 	int numerorighe=8;
 	int numeropezzi=32;
 	public ArrayList<Pezzo> pezzidelgioco;
+	
 	Cella[][] scacchiera;
 
 
@@ -20,12 +21,15 @@ public class Scacchiera {
 	public Cella getCella(int x,int y) {
 		return scacchiera[x][y];
 	}
+	public void setCella(Cella nuovaCella,int x,int y) {
+		scacchiera[x][y]=nuovaCella;
+	}
 	public Scacchiera(){
 
 		pezzidelgioco = new ArrayList<Pezzo>();
 		scacchiera = new Cella[numerocolonne][numerorighe];
-		for(int i=0;i<8;i++) {
-			for(int j=0;j<8;j++) {
+		for(int i=0;i<getNumeroColonne();i++) {
+			for(int j=0;j<getNumeroRighe();j++) {
 				scacchiera[i][j]=new Cella(i,j,null);
 			}
 		}
