ce8ddf0b7f9f9f4357a4bd820894159a3d63d6c0
ggiorgino3 <g.giorgino3@studenti.uniba.it>
2020-04-12 01:16:32 +0200

    Aggiunta classe Controller

---
+++ src/main/java/it/uniba/main/Controller.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.uniba.main;
+
+/**
+ * Classe che gestisce le varie funzionalità del gioco.
+ */
+public class Controller {
+	
+	/**
+	 * 
+	 */
+	public Controller() {
+
+	}
+	
+	
+
+}
