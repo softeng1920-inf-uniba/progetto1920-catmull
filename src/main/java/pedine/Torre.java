5b24a88fc40412af87e88d38caadc63c3166a855
NunziaL <62551008+NunziaL@users.noreply.github.com>
2020-04-15 17:26:47 +0200

    Update Torre.java

--- src/main/java/pedine/Torre.java
+++ src/main/java/pedine/Torre.java
@@ -7,11 +7,7 @@ import scacchiera.Cella;
  * Realizzazione della classe Torre, estensione della classe Pezzo
  */
 public final class Torre extends Pezzo {
-
-<<<<<<< HEAD
-	/** Costruttore */
-=======
->>>>>>> 8d0cc484439433782260394a1b83626179e0ca53
+	
 	public Torre(final Colore colore, final Cella posizioneCorrente) {
 		super("Torre", colore, posizioneCorrente);
 		if (colore == Colore.nero) {
