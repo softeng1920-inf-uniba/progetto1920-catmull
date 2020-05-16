bcf1776b4d86e056ef9d0d8d4a1b8c31da1db9f5
antoniocurci06 <anto.curci99@gmail.com>
2020-05-16 10:18:11 +0200

    Creazione test automatici per Cavallo e Pedone

---
+++ src/main/java/test/ReTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReTest {
+
+	@Test
+	void testIsMossaValida() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsArroccoValido() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCoordinateArrocco() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConvertiMossa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsReSottoScacco() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsPrimaMossaEffettuata() {
+		fail("Not yet implemented");
+	}
+
+}
