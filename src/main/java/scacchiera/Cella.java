87d464e0c49bdb4fdd0f25dea1a535c0098ecc0d
mcalvano5 <miry.calvano1999@libero.it>
2020-05-16 13:03:17 +0200

    Aggiunta implementazione TestCella #92

--- src/main/java/scacchiera/Cella.java
+++ src/main/java/scacchiera/Cella.java
@@ -208,5 +208,6 @@ public final class Cella {
     }
 
 
+
 }
 
--- src/main/java/scacchiera/Scacchiera.java
+++ src/main/java/scacchiera/Scacchiera.java
@@ -70,6 +70,7 @@ public final class Scacchiera {
 		return scacchiera[x][y];
 	}
 
+
 	/**
 	 * Controlla che le coordinate in input siano valide per la scacchiera, ovvero
 	 * non vadano oltre i limiti.
