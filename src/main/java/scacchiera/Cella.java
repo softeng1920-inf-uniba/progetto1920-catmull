f715276835554619ae1ff1ec2659a7a1bf299a66
mcalvano5 <miry.calvano1999@libero.it>
2020-05-16 12:36:16 +0200

    Aggiunta implementazione TestRegina #92

--- src/main/java/scacchiera/Cella.java
+++ src/main/java/scacchiera/Cella.java
@@ -7,168 +7,167 @@ import scacchiera.pedine.Pezzo;
  * scacchiera, definendola con gli attributi: x indica la coordinata delle
  * ascisse, y indica la coordinata delle ordinate, occupato indica lo stato
  * della cella e pezzoCorrente da informazioni riguardo al pezzo contenuto nella
- * cella.
- * La classe Cella e' di tipo ENTITY
+ * cella. La classe Cella e' di tipo ENTITY
  */
 public final class Cella {
-    private int x;
-    private int y;
-    private boolean occupato; // TODO: lo stato occupato si puo' ottenere dalla cella, se contiene un pezzo o
-			      // meno. Si potrebbe pensare di eliminare questa variabile
-    private Pezzo pezzoCorrente;
+	private int x;
+	private int y;
+	private boolean occupato; // TODO: lo stato occupato si puo' ottenere dalla cella, se contiene un pezzo o
+	// meno. Si potrebbe pensare di eliminare questa variabile
+	private Pezzo pezzoCorrente;
 
-    /** Costuttore */
-    public Cella(final int x, final int y, final Pezzo pezzoCorrente) {
-	this.x = x;
-	this.y = y;
-	this.pezzoCorrente = pezzoCorrente;
-    }
-
-    // ---------Metodi di setting---------
-
-    /** Modifica la variabile occupato */
-    public void setOccupato(final boolean occupato) {
-	this.occupato = occupato;
-    }
-
-    /** Modifica il tipo di pezzo che contiene */
-    void setPezzoCorrente(final Pezzo pezzoCorrente) {
-	this.pezzoCorrente = pezzoCorrente;
-	if (pezzoCorrente != null) {
-	    this.occupato = true;
+	/** Costuttore */
+	public Cella(final int x, final int y, final Pezzo pezzoCorrente) {
+		this.x = x;
+		this.y = y;
+		this.pezzoCorrente = pezzoCorrente;
 	}
-    }
 
-    /** Modifica la coordinata x */
-    void setX(final int newx) {
-	x = newx;
-    }
+	// ---------Metodi di setting---------
 
-    /** Modifica la coordinata y */
-    void setY(final int newy) {
-	y = newy;
-    }
+	/** Modifica la variabile occupato */
+	public void setOccupato(final boolean occupato) {
+		this.occupato = occupato;
+	}
 
-    // --------Metodi di Get--------
+	/** Modifica il tipo di pezzo che contiene */
+	void setPezzoCorrente(final Pezzo pezzoCorrente) {
+		this.pezzoCorrente = pezzoCorrente;
+		if (pezzoCorrente != null) {
+			this.occupato = true;
+		}
+	}
 
-    /** Restituisce la coordinata x */
-    public int getX() {
-	return x;
-    }
+	/** Modifica la coordinata x */
+	void setX(final int newx) {
+		x = newx;
+	}
 
-    /** Restituisce la coordinata y */
-    public int getY() {
-	return y;
-    }
+	/** Modifica la coordinata y */
+	void setY(final int newy) {
+		y = newy;
+	}
 
-    /** Restituisce la variabile occupato */
-    public boolean isOccupato() {
-	return occupato;
-    }
+	// --------Metodi di Get--------
 
-    /** Restituisce il pezzo che contiene */
-    public Pezzo getPezzoCorrente() {
-	return pezzoCorrente;
-    }
+	/** Restituisce la coordinata x */
+	public int getX() {
+		return x;
+	}
 
-    // ------Metodi per aggiugere o togliere pezzi dalla scacchiera
+	/** Restituisce la coordinata y */
+	public int getY() {
+		return y;
+	}
 
-    /** Aggiunge un pezzo alla cella */
-    public void aggiungiPezzo(final Pezzo nuovoPezzo) {
+	/** Restituisce la variabile occupato */
+	public boolean isOccupato() {
+		return occupato;
+	}
 
-	setPezzoCorrente(nuovoPezzo);
-	setOccupato(true);
+	/** Restituisce il pezzo che contiene */
+	public Pezzo getPezzoCorrente() {
+		return pezzoCorrente;
+	}
 
-    }
+	// ------Metodi per aggiugere o togliere pezzi dalla scacchiera
 
-    /** Rimuove il pezzo nella cella */
-    public void rimuoviPezzoCorrente() {
-	setOccupato(false);
-	setPezzoCorrente(null);
-    }
+	/** Aggiunge un pezzo alla cella */
+	public void aggiungiPezzo(final Pezzo nuovoPezzo) {
 
-    /**
-     * @param char coordX Carattere in minuscolo da convertire in intero
-     * @return int Valore necessario per la scacchiera compreso fra 0 e 7
-     */
-    public static int coordXinInt(final char coordX) {
-	return coordX - 97;
-    }
+		setPezzoCorrente(nuovoPezzo);
+		setOccupato(true);
 
-    /**
-     * Restituisce il corrispondente valore della scacchiera
-     *
-     * @param int coordY Compreso fra 1 e 8
-     * @return int Coordinata convertita in intero, compreso tra 0 e 7
-     */
-    public static int coordYinInt(final char coordY) {
+	}
 
-	return Math.abs((coordY - 49) - 7);
-    }
+	/** Rimuove il pezzo nella cella */
+	public void rimuoviPezzoCorrente() {
+		setOccupato(false);
+		setPezzoCorrente(null);
+	}
 
-    /**
-     * @param char coordX intero da convertire in carattere
-     * @return char Valore necessario per la scacchiera compreso fra a e h
-     */
+	/**
+	 * @param char coordX Carattere in minuscolo da convertire in intero
+	 * @return int Valore necessario per la scacchiera compreso fra 0 e 7
+	 */
+	public static int coordXinInt(final char coordX) {
+		return coordX - 97;
+	}
 
-    public static char coordXinChar(final int coordX) {
+	/**
+	 * Restituisce il corrispondente valore della scacchiera
+	 *
+	 * @param int coordY Compreso fra 1 e 8
+	 * @return int Coordinata convertita in intero, compreso tra 0 e 7
+	 */
+	public static int coordYinInt(final char coordY) {
 
-	return (char) (coordX + 97);
+		return Math.abs((coordY - 49) - 7);
+	}
 
-    }
+	/**
+	 * @param char coordX intero da convertire in carattere
+	 * @return char Valore necessario per la scacchiera compreso fra a e h
+	 */
 
-    /**
-     *
-     * @param char coordY intero da convertire in carattere
-     *
-     * @return char Valore necessario per la scacchiera compreso fra 1 e 8
-     *
-     */
+	public static char coordXinChar(final int coordX) {
 
-    public static char coordYinChar(final int coordY) {
+		return (char) (coordX + 97);
 
-	return (char) (Math.abs((coordY - 8)) + 48);
+	}
 
-    }
+	/**
+	 *
+	 * @param char coordY intero da convertire in carattere
+	 *
+	 * @return char Valore necessario per la scacchiera compreso fra 1 e 8
+	 *
+	 */
 
-    /**
-     * Converte la coordinata X di partenza data in input in intero.
-     *
-     * @param m
-     * @return
-     */
-    public static int startX(String m) {
-	return Cella.coordXinInt(m.charAt(0));
-    }
+	public static char coordYinChar(final int coordY) {
 
-    /**
-     * Converte la coordinata Y di partenza data in input in intero.
-     *
-     * @param m
-     * @return
-     */
-    public static int startY(String m) {
-	return Cella.coordYinInt(m.charAt(1));
-    }
+		return (char) (Math.abs((coordY - 8)) + 48);
 
-    /**
-     * Converte la coordinata X di partenza data in input in intero.
-     *
-     * @param m
-     * @return
-     */
-    public static int endX(String m) {
-	return Cella.coordXinInt(m.charAt(3));
-    }
+	}
 
-    /**
-     * Converte la coordinata Y di partenza data in input in intero.
-     *
-     * @param m
-     * @return
-     */
-    public static int endY(String m) {
-	return Cella.coordYinInt(m.charAt(4));
-    }
+	/**
+	 * Converte la coordinata X di partenza data in input in intero.
+	 *
+	 * @param m
+	 * @return
+	 */
+	public static int startX(String m) {
+		return Cella.coordXinInt(m.charAt(0));
+	}
+
+	/**
+	 * Converte la coordinata Y di partenza data in input in intero.
+	 *
+	 * @param m
+	 * @return
+	 */
+	public static int startY(String m) {
+		return Cella.coordYinInt(m.charAt(1));
+	}
+
+	/**
+	 * Converte la coordinata X di partenza data in input in intero.
+	 *
+	 * @param m
+	 * @return
+	 */
+	public static int endX(String m) {
+		return Cella.coordXinInt(m.charAt(3));
+	}
+
+	/**
+	 * Converte la coordinata Y di partenza data in input in intero.
+	 *
+	 * @param m
+	 * @return
+	 */
+	public static int endY(String m) {
+		return Cella.coordYinInt(m.charAt(4));
+	}
 
 }
